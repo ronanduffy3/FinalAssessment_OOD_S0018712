33061bf2b39f83aeb7cbeed1eda74611df80e639
Ronan Duffy <s00187127@mail.itsligo.ie>
2020-05-13 12:28:53 +0100

    Added in os images

--- FinalAssessment_OOD_Ronan_Finnegan_Duffy_S00187127/MainWindow.xaml.cs
+++ FinalAssessment_OOD_Ronan_Finnegan_Duffy_S00187127/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -29,15 +30,40 @@ namespace FinalAssessment_OOD_Ronan_Finnegan_Duffy_S00187127
 
         private void Grid_Loaded(object sender, RoutedEventArgs e)
         {
+
+            // Query to read the phones from the database
             var query = (from Phone in db.phones
                          select Phone).ToList();
 
+            // Run a loop to add all the phones found in the query
             foreach(var phone in query)
             {
                 phoneList.Add(phone);
             }
 
+            // Set that to the item source
             listBox.ItemsSource = phoneList;
+        }
+
+        private void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Cast the selected item to a phone object
+            Phone selectedItem = listBox.SelectedItem as Phone;
+
+            // Get the phone_image location from the database
+            string img = selectedItem.Phone_Image.ToString();
+
+            var imgN = new BitmapImage();
+
+            imgN.BeginInit();
+            imgN.UriSource = new Uri(selectedItem.Phone_Image.ToString(), UriKind.Relative);
+            imgN.EndInit();
+
+            imgPhone.Source = imgN;
+
+            // Set the text to the cost of the phone
+            tblkPrice.Text = selectedItem.Price.ToString("C");
+
         }
     }
 }
